390514e2413da13ce87155308faa4cea03c75a71
Artur <arthur.kolishenko@gmail.com>
2020-11-03 20:14:11 +0100

    bpe model integrated with indexer

--- test/test_indexer_new.py
+++ test/test_indexer_new.py
@@ -27,4 +27,17 @@ class TestIndexer(unittest.TestCase):
             self.assertTrue(res[0][1][0]<1e-5) # find itself, distance apx. 0
 
     def test_bpe_indexer(self):
-        pass
+        from ml_models.bpe_model import indexer
+        for q in questions:
+            r = indexer.model.sentence_embedding(q)
+            self.assertTrue(r.shape[0]==300) # embedding dimension
+
+            res = indexer.index.knnQueryBatch(queries=[r], k=4, num_threads=2)
+            self.assertTrue(len(res[0][0])==4) # return 4 neighbours
+            self.assertTrue(res[0][1][0]<1e-5) # find itself, distance apx. 0
+    
+def main():
+    tester = TestIndexer()
+    tester.test_bert_indexer()
+    tester.test_use_indexer()
+    tester.test_bpe_indexer()
