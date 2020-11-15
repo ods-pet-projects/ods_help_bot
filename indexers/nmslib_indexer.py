a54d6eecf5a8fec04a68099f3698dd7fad26381d
Artur <arthur.kolishenko@gmail.com>
2020-11-15 18:17:15 +0100

    hard inputed model names changed to support_model's model names

--- indexers/nmslib_indexer.py
+++ indexers/nmslib_indexer.py
@@ -5,20 +5,21 @@ from tqdm import tqdm
 tqdm.pandas()
 from text_utils.utils import prepare_ans
 from utils.base_classes import BaseIndexer
+from support_model import ModelNames
 from config import ifile_train_path, INDEX_DIR, index_path
 
 MAX_TEXT_LEN = 300
 df = pd.read_csv(ifile_train_path)
 
-class UniIndexer(BaseIndexer):
+class NMSlibIndexer(BaseIndexer):
     def __init__(self, model_name, logger):
-        if model_name == 'bert':
+        if model_name == ModelNames.BERT:
             from ml_models.bert_model import BertEmbedder
             self.model = BertEmbedder()
-        elif model_name == 'use':
+        elif model_name == ModelNames.USE:
             from ml_models.use_model import STEmbedder
             self.model = STEmbedder()
-        elif model_name == 'bpe':
+        elif model_name == ModelNames.BPE:
             from ml_models.bpe_model import BPEEmbedder
             self.model = BPEEmbedder()
         else:
@@ -62,7 +63,7 @@ class UniIndexer(BaseIndexer):
 
     def make_data_embeddings(self, data):
         names_sparse_matrix = []
-        for i, text_prepared in tqdm(enumerate(data)):
+        for _, text_prepared in tqdm(enumerate(data)):
             names_sparse_matrix.append(self.model.sentence_embedding(text_prepared))
         self.logger.info('make embeddings finished')
         self.logger.info('pos count %s', self.model.success_count)
@@ -78,7 +79,7 @@ class UniIndexer(BaseIndexer):
             raise IndexError("Index is not yet created or loaded")
 
 def prepare_indexer(model_name, logger):
-    indexer = UniIndexer(model_name, logger)
+    indexer = NMSlibIndexer(model_name, logger)
     indexer.create_index(index_path[model_name], df['text'].values)
     return indexer, df
 
